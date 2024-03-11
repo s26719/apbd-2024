9d73237a29a6b4acc77b68d82e5311e2ceb7f69e
s26719 <s26719@pjwstk.edu.pl>
2024-03-11 18:10:39 +0100

    Modyfikacja1

--- zad1/zad1/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿
-
-Console.WriteLine("Hello, World!");
-
-    int[] tab1 = {1,2,3,4,5};
-    
--- zadd1/zadd1/Program.cs
+++ zadd1/zadd1/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+int[] tab1 = {1,2,3,4,5};
